05fa4413bbebe80dac0ca21e7922381ade5a833f
Bulakorn Junboonmee <6310682577@cn330>
2023-03-26 00:55:14 +0700

    add video and finish

--- QuizzApp/app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ QuizzApp/app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.quizzapp
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.booleanResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,11 +39,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
 
-    var numberGen = (0 .. 9).random()
-    var randomIndex by remember {
-        mutableStateOf(numberGen)
+    var indexList by remember {
+        mutableStateOf(mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
     }
-    var res = ""
+    var randomIndex by remember {
+        mutableStateOf(indexList.random())
+    }
     val allQuestions = mutableListOf(
         "2 + 5 = ?",
         "3 x 2 = ?",
@@ -56,9 +57,11 @@ fun MainScreen(modifier: Modifier = Modifier) {
         "19 + 5 = ?",
         "26 - 3 = ?"
     )
-
-    allQuestions.shuffle()
-
+    var question by remember { mutableStateOf(allQuestions[randomIndex]) }
+    var correctAnswer by remember { mutableStateOf(answers(randomIndex)[0]) }
+    var answer by remember { mutableStateOf(answers(randomIndex)) }
+    var check by remember { mutableStateOf("none") }
+    var score by remember { mutableStateOf(0) }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -70,89 +73,118 @@ fun MainScreen(modifier: Modifier = Modifier) {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(32.dp))
-
-        Button(
-            onClick = {randomIndex = (0 until(allQuestions.size)).random() }) {
-            Text(text = "Next Question")
+        if (indexList.isEmpty()) {
+            Text(
+                text = "Finish",
+                fontSize = 24.sp)
+            Spacer(Modifier.height(10.dp))
+            Text(
+                text = "Score = $score/10",
+                fontSize = 24.sp)
+            Spacer(Modifier.height(10.dp))
+            Button(onClick = {
+                indexList = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                score = 0;
+                check = "none"
+            }) {
+                Text(text = "Reset")
             }
-        Spacer(Modifier.height(32.dp))
-
-        Text(
-            text = allQuestions[randomIndex],
-            fontSize = 24.sp
-        )
-
-        Spacer(Modifier.height(32.dp))
-        Text(text = res)
-        if (allQuestions[randomIndex] == "2 + 5 = ?") {
-            res = "true"
-            Text(text = res)
         }
         else {
-            res = "false"
-            Text(text = res)
+            Button(
+                onClick = {
+                    indexList.remove(randomIndex);
+                    randomIndex = if (indexList.isEmpty()) 0 else indexList.random();
+                    question = allQuestions[randomIndex];
+                    correctAnswer = answers(randomIndex)[0];
+                    answer = answers(randomIndex);
+                    answer.shuffle()
+                }) {
+                Text(text = "Next Question")
+            }
+            Spacer(Modifier.height(32.dp))
+            Text(
+                text = "${11 - indexList.count()}. " + question,
+                fontSize = 24.sp
+            )
+            Spacer(Modifier.height(16.dp))
+            /*choice 1*/
+            Button(onClick = {
+                check = if (answer[0] == correctAnswer) "true" else "false";
+                indexList.remove(randomIndex);
+                randomIndex = if (indexList.isEmpty()) 0 else indexList.random();
+                question = allQuestions[randomIndex];
+                correctAnswer = answers(randomIndex)[0];
+                answer = answers(randomIndex);
+                answer.shuffle()
+            }) {
+                Text(text = answer[0])
+            }
+
+            /*choice 2*/
+            Button(
+                onClick = {
+                    check = if (answer[1] == correctAnswer) "true" else "false";
+                    indexList.remove(randomIndex);
+                    randomIndex = if (indexList.isEmpty()) 0 else indexList.random();
+                    question = allQuestions[randomIndex];
+                    correctAnswer = answers(randomIndex)[0];
+                    answer = answers(randomIndex);
+                    answer.shuffle()
+                }) {
+                Text(text = answer[1])
+            }
+
+            /*choice 3*/
+            Button(onClick = {
+                check = if (answer[2] == correctAnswer) "true" else "false";
+                indexList.remove(randomIndex);
+                randomIndex = if (indexList.isEmpty()) 0 else indexList.random();
+                question = allQuestions[randomIndex];
+                correctAnswer = answers(randomIndex)[0];
+                answer = answers(randomIndex);
+                answer.shuffle()
+            }) {
+                Text(text = answer[2])
+            }
+
+            /*choice 4*/
+            Button(onClick = {
+                check = if (answer[3] == correctAnswer) "true" else "false";
+                indexList.remove(randomIndex);
+                randomIndex = if (indexList.isEmpty()) 0 else indexList.random();
+                question = allQuestions[randomIndex];
+                correctAnswer = answers(randomIndex)[0];
+                answer = answers(randomIndex);
+                answer.shuffle()
+            }) {
+                Text(text = answer[3])
+            }
+
+            if (check == "true") {
+                score += 1
+            }
+
+            check = "none"
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = "Score = $score",
+                fontSize = 24.sp
+            )
         }
-
-
     }
-    
-    
-
 }
 
 
-fun ShuffleAnswers(modifier: Modifier = Modifier){
-    val answer1 = mutableListOf(
-        "7","14","11","5"
+
+fun answers(index: Int): MutableList<String> {
+    val answerList = mutableListOf("7", "6", "6", "7", "13", "52", "2", "112", "24", "23")
+    return mutableListOf(
+        answerList[index],
+        (80..120).random().toString(),
+        (40..80).random().toString(),
+        (0..40).random().toString()
     )
-
-    val answer2 = mutableListOf(
-        "6","15","20","25"
-    )
-
-    val answer3 = mutableListOf(
-        "6","15","20","25"
-    )
-
-    val answer4 = mutableListOf(
-        "7","15","20","25"
-    )
-
-    val answer5 = mutableListOf(
-        "13","15","20","25"
-    )
-
-    val answer6 = mutableListOf(
-        "52","15","20","25"
-    )
-
-    val answer7 = mutableListOf(
-        "2","15","20","25"
-    )
-
-    val answer8 = mutableListOf(
-        "112","15","20","25"
-    )
-
-    val answer9 = mutableListOf(
-        "24","15","20","25"
-    )
-
-    val answer10 = mutableListOf(
-        "23","15","20","25"
-    )
-
-    answer1.shuffle()
-    answer2.shuffle()
-    answer3.shuffle()
-    answer4.shuffle()
-    answer5.shuffle()
-    answer6.shuffle()
-    answer7.shuffle()
-    answer8.shuffle()
-    answer9.shuffle()
-    answer10.shuffle()
-
 }
 
 @Preview(showBackground = true)
